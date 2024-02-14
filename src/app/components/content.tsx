24b9b48911fe4523e19077906930ffb052c853cd
samgeosa <samuel.sasaki@student.president.ac.id>
2024-02-14 20:33:09 +0700

    first commit

---
+++ src/app/components/content.tsx
@@ -0,0 +1,22 @@
+export default function Content() {
+  return (
+    <div className="flex-1">
+      <div className="flex min-h-screen w-full items-center justify-center bg-blue-500">
+        <div className="flex flex-col text-center">
+          <h1 className="text-8xl font-medium text-white poppins">B R U</h1>
+          <p className="text-2xl text-white poppins pt-5">
+            buru - buru? BRU aja!
+          </p>
+        </div>
+      </div>
+      <div className="min-h-screen flex w-full items-center justify-center bg-white flex-col poppins">
+        <div className="max-w-7xl">
+          <h1 className="text-3xl font-bold text-center text-[#0082FF] pb-5">About Us</h1>
+          <p className="text-xl text-justify">
+          We are BRU, a revolutionary online transportation solution that understands your mobility needs. We take pride in offering transportation services that go beyond just a journey, providing a comfortable, efficient, and safe experience. With a fleet of the latest vehicles and a team of professional drivers, we are committed to delivering reliable and high-quality services. BRU is not just a transportation platform; it is also a loyal companion on every journey. With continuous innovation and a focus on customer satisfaction, we strive to create a better future for mobility. Join us and enjoy hassle-free travel with BRU. Choose something else? That&apos;s right, loss!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ['400', '500', '600', '700', '100', '200', '300', '800', '900'],  
+  variable: '--font-poppins',
+});
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.variable}>{children}</body>
     </html>
   );
 }
